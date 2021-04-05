8a53572991ed460540d03f42605b6b82cdfd30e2
Enoch Chu <git@enochchu.com>
2021-04-04 23:32:51 -0700

    Remove blog

--- src/react-enoch-github-io/src/App.js
+++ src/react-enoch-github-io/src/App.js
@@ -32,16 +32,11 @@ class App extends Component {
 					<Router>
 						<span>
 							<Link to="/">Home</Link>
-							<Link to="/blog">Blog</Link>
 							<Link to="/github">Github</Link>
 						</span>
 						<Switch>
 							<Route path='/github' component={() => { 
 								window.location.href = 'https://github.com/enochchu'; 
-								return null;
-							}} />
-							<Route path='/blog' component={() => { 
-								window.location.href = 'https://enochchu.github.io/blog/'; 
 								return null;
 							}} />
 						</Switch>
