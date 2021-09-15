0aeacc9b0b8b66d782cc7678e60940fa2f248ab1
Enoch Chu <git@enochchu.com>
2021-09-14 21:10:33 -0700

    Initial Commit for Blog Component

---
+++ src/react-enoch-github-io/src/components/blog/Blog.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+import "./Blog.css"
+
+class Blog extends Component {
+    render() {
+        return (
+            <div className="blog">
+                This is a blog component.
+                <br />
+                This is a blog component.
+                <br />
+                This is a blog component.
+                <br />
+                This is a blog component.
+                <br />
+                This is a blog component.
+            </div>
+        )
+    }
+}
+
+export default Blog;
