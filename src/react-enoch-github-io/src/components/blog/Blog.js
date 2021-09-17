de944f6aec4815bdfb354abbcdc6c4417317e102
Enoch Chu <git@enochchu.com>
2021-09-16 22:40:58 -0700

    MVP Just show some markdown stuff

--- src/react-enoch-github-io/src/components/blog/Blog.js
+++ src/react-enoch-github-io/src/components/blog/Blog.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 
 import "./Blog.css"
+import post from "./../../resources/posts/Hello-World.md"
+
+import marked from "marked";
 
 class Blog extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: ""
+        }
+    }
+    componentDidMount() {
+        fetch(post)
+            .then((data) => {
+                return data.text()
+            })
+            .then(((data) => {
+                this.setState((state, props) => {
+                    return {
+                        data: marked(data),
+                        ...props
+                    }
+                });
+            }));
+    }
+
+    get_post() {
+        return {
+            __html: this.state.data
+        }
+    }
+
     render() {
         return (
             <div className="blog">
-                This is a blog component.
-                <br />
-                This is a blog component.
-                <br />
-                This is a blog component.
-                <br />
-                This is a blog component.
-                <br />
-                This is a blog component.
+                <div dangerouslySetInnerHTML={this.get_post()} />
             </div>
         )
     }
