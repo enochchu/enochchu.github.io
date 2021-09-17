3a88245f68c9936d49c5728da45a18cc24d91a92
Enoch Chu <git@enochchu.com>
2021-09-16 21:40:15 -0700

    Setting up Jasmine

---
+++ src/react-enoch-github-io/spec/features/sanitycheck.spec.js
@@ -0,0 +1,9 @@
+describe("Test Suite Sanity Check", () => {
+    it("should run successfully", () => {
+        expect(true).toBeTrue();
+    });
+
+    it('should fail', () => {
+        expect(false).toBeFalse();
+    });
+});
